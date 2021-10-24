fab50de2601d5307cad87578ce2b4864cd4a7ba1
Philipp Albrecht <palbrecht@mailbox.org>
2021-10-24 11:22:44 +0200

    Implement Wire::from_instructions()

--- 2019/src/day03.rs
+++ 2019/src/day03.rs
@@ -1,13 +1,17 @@
 use std::ops::Add;
 
-enum Step {
-    Up(isize),
-    Down(isize),
-    Left(isize),
-    Right(isize),
+pub fn run() {
 }
 
-#[derive(Debug, PartialEq)]
+#[derive(Debug)]
+enum Step {
+    Up(u32),
+    Down(u32),
+    Left(u32),
+    Right(u32),
+}
+
+#[derive(Debug, PartialEq, Clone, Copy)]
 struct Point {
     x: isize,
     y: isize,
@@ -24,11 +28,43 @@ impl Add<Step> for Point {
 
     fn add(self, step: Step) -> Self::Output {
         match step {
-            Step::Up(count) => Point::new(self.x, self.y + count),
-            Step::Down(count) => Point::new(self.x, self.y - count),
-            Step::Left(count) => Point::new(self.x - count, self.y),
-            Step::Right(count) => Point::new(self.x + count, self.y),
+            Step::Up(count) => Point::new(self.x, self.y + count as isize),
+            Step::Down(count) => Point::new(self.x, self.y - count as isize),
+            Step::Left(count) => Point::new(self.x - count as isize, self.y),
+            Step::Right(count) => Point::new(self.x + count as isize, self.y),
         }
+    }
+}
+
+struct Wire {
+    path: Vec<Point>,
+}
+
+impl Wire {
+    fn from_instructions(instructions: &str) -> Wire {
+        let mut cursor = Point::new(0, 0);
+        let mut path = vec![cursor];
+
+        for instruction in instructions.split(',') {
+            let direction = instruction.chars().nth(0).unwrap();
+            let count = instruction.chars().nth(1).unwrap();
+
+            let step = match direction {
+                'U' => Step::Up(count.to_digit(10).unwrap()),
+                'D' => Step::Down(count.to_digit(10).unwrap()),
+                'L' => Step::Left(count.to_digit(10).unwrap()),
+                'R' => Step::Right(count.to_digit(10).unwrap()),
+                _ => panic!("unknown direction: {}", direction),
+            };
+            cursor = cursor + step;
+            path.push(cursor);
+        }
+
+        Wire { path }
+    }
+
+    fn path(&self) -> &[Point] {
+        &self.path
     }
 }
 
@@ -41,5 +77,12 @@ mod tests {
         let point = Point::new(0, 0);
         let step = Step::Up(5);
         assert_eq!(point + step, Point::new(0, 5));
+    }
+
+    #[test]
+    fn create_wire_from_instructions() {
+        let instructions = "U8,R4";
+        let wire = Wire::from_instructions(instructions);
+        assert_eq!(wire.path(), [Point::new(0, 0), Point::new(0, 8), Point::new(4, 8)]);
     }
 }
